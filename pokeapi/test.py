fe2e857ff33c88a35b75ed194f66b6e35b0c95c1
cvelazqueze <ing.cesarvelazquez@gmail.com>
2022-01-03 22:08:22 -0600

    initial commit

---
+++ pokeapi/test.py
@@ -0,0 +1,11 @@
+import json
+
+from django.test import TestCase
+
+class PokemonEndpointTestCase(TestCase):
+    
+    def test_get(self):
+        """
+        Test that we can get a list of Pokemon
+        """
+        self.fail('Write the test')
