41e8c2e0389f27b2e5082410051aca32dd5e7da3
Cesar Velazquez <ing.cesarvelazquez@gmail.com>
2022-01-03 23:32:17 -0600

    Working API with local data

---
+++ pokemon/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Pokemon, Attack
+
+
+class AttackInline(admin.TabularInline):
+    model = Attack
+
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    inlines = (AttackInline,)
